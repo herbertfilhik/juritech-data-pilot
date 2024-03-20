527ecc1b8f0979d18c039bb1253f80977b339496
herbertfilhik <herbertfilhik@gmail.com>
2024-03-20 18:06:08 -0300

    update to disable specific button to tests

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,26 @@ import './Home.css'; // Certifique-se de que o caminho está correto
 
 function Home() {
   return (
+    // <div className="home-container">
+    //  <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
+    //  <Link to="/" className="btn btn-primary link-vertical">Menu</Link>
+    //  <Link to="/upload" className="btn btn-primary link-vertical">Realizar Upload da Operação</Link>
+    //  <Link to="/fluxo" className="btn btn-primary link-vertical">Acompanhar Serviço</Link>
+    //  <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}      
+    //  <Link to="/excluiracompservico" className="btn btn-primary link-vertical">Excluir Tabela da Operação</Link>
+    //  <Link to="/logout" className="btn btn-primary link-vertical">Logout</Link>
+    // </div>
+
     <div className="home-container">
       <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
-      <Link to="/" className="btn btn-primary link-vertical">Menu</Link>
-      <Link to="/upload" className="btn btn-primary link-vertical">Realizar Upload da Operação</Link>
+      <Link to="/" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Menu</Link>
+      <Link to="/upload" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Realizar Upload da Operação</Link>
       <Link to="/fluxo" className="btn btn-primary link-vertical">Acompanhar Serviço</Link>
-      <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}      
-      <Link to="/excluiracompservico" className="btn btn-primary link-vertical">Excluir Tabela da Operação</Link>
+      <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}
+      <Link to="/excluiracompservico" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Excluir Tabela da Operação</Link>
       <Link to="/logout" className="btn btn-primary link-vertical">Logout</Link>
     </div>
+
   );
 }
 
