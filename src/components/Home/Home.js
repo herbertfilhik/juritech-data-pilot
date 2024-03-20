2aa558280be5c16fcdc4e43f6144afabe1fdc3a6
herbertfilhik <herbertfilhik@gmail.com>
2024-03-20 19:52:12 -0300

    change disable

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,26 +4,15 @@ import './Home.css'; // Certifique-se de que o caminho está correto
 
 function Home() {
   return (
-    // <div className="home-container">
-    //  <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
-    //  <Link to="/" className="btn btn-primary link-vertical">Menu</Link>
-    //  <Link to="/upload" className="btn btn-primary link-vertical">Realizar Upload da Operação</Link>
-    //  <Link to="/fluxo" className="btn btn-primary link-vertical">Acompanhar Serviço</Link>
-    //  <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}      
-    //  <Link to="/excluiracompservico" className="btn btn-primary link-vertical">Excluir Tabela da Operação</Link>
-    //  <Link to="/logout" className="btn btn-primary link-vertical">Logout</Link>
-    // </div>
-
     <div className="home-container">
       <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
-      <Link to="/" className="btn btn-primary link-vertical" onclick="return false;">Menu</Link>      
-      <Link to="/upload" className="btn btn-primary link-vertical" onClick="return false;">Realizar Upload da Operação</Link>
+      <Link to="/" className="btn btn-primary link-vertical">Menu</Link>
+      <Link to="/upload" className="btn btn-primary link-vertical">Realizar Upload da Operação</Link>
       <Link to="/fluxo" className="btn btn-primary link-vertical">Acompanhar Serviço</Link>
-      <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}
-      <Link to="/excluiracompservico" className="btn btn-primary link-vertical" onClick="return false;">Excluir Tabela da Operação</Link>
+      <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}      
+      <Link to="/excluiracompservico" className="btn btn-primary link-vertical">Excluir Tabela da Operação</Link>
       <Link to="/logout" className="btn btn-primary link-vertical">Logout</Link>
     </div>
-
   );
 }
 
