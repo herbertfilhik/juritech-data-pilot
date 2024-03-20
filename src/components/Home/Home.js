7663fe71ed5bada63cf1ea34ee63f1c686434679
herbertfilhik <herbertfilhik@gmail.com>
2024-03-20 19:41:06 -0300

    change disable

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './Home.css'; // Certifique-se de que o caminho está correto
 
 function Home() {
+
   return (
     // <div className="home-container">
     //  <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
@@ -15,15 +16,14 @@ function Home() {
     // </div>
 
     <div className="home-container">
-      <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
-      <Link to="/" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Menu</Link>
-      <Link to="/upload" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Realizar Upload da Operação</Link>
+      <h1>Bem-vindo ao JuriTech - Data Pilot</h1>      
+      <Link to="/" className="btn btn-primary btn-disabled link-vertical">Menu</Link>
+      <Link to="/upload" className={`btn btn-primary link-vertical ${isUploadDisabled ? 'btn-disabled' : ''}`}>Realizar Upload da Operação</Link>
       <Link to="/fluxo" className="btn btn-primary link-vertical">Acompanhar Serviço</Link>
-      <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}
-      <Link to="/excluiracompservico" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Excluir Tabela da Operação</Link>
+      <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link>
+      <Link to="/excluiracompservico" className="btn btn-primary link-vertical btn-disabled">Excluir Tabela da Operação</Link>
       <Link to="/logout" className="btn btn-primary link-vertical">Logout</Link>
     </div>
-
   );
 }
 
