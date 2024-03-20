6c84e9bc7273e2310813ed340c4140e5fea56526
herbertfilhik <herbertfilhik@gmail.com>
2024-03-20 19:48:04 -0300

    change disable

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,11 +16,11 @@ function Home() {
 
     <div className="home-container">
       <h1>Bem-vindo ao JuriTech - Data Pilot</h1>
-      <Link to="/" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Menu</Link>
-      <Link to="/upload" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Realizar Upload da Operação</Link>
+      <Link to="/" className="btn btn-primary link-vertical" onclick="return false;">Menu</Link>      
+      <Link to="/upload" className="btn btn-primary link-vertical" onClick="return false;">Realizar Upload da Operação</Link>
       <Link to="/fluxo" className="btn btn-primary link-vertical">Acompanhar Serviço</Link>
       <Link to="/incluir-excluir" className="btn btn-primary link-vertical">Incluir e Excluir Operação</Link> {/* Nova opção adicionada */}
-      <Link to="/excluiracompservico" className="btn btn-primary link-vertical" onClick={(e) => e.preventDefault()}>Excluir Tabela da Operação</Link>
+      <Link to="/excluiracompservico" className="btn btn-primary link-vertical" onClick="return false;">Excluir Tabela da Operação</Link>
       <Link to="/logout" className="btn btn-primary link-vertical">Logout</Link>
     </div>
 
